e8fe7a73852ef6d7156e4f91f809c594e61bedf7
Colin Sullivan <colin@synadia.com>
2023-01-10 17:38:36 -0700

    add natsthru
    Signed-off-by: Colin Sullivan <colin@synadia.com>

--- install_tools.sh
+++ install_tools.sh
@@ -11,5 +11,9 @@ curl -sLJO https://github.com/nats-io/nats-top/releases/download/v0.5.3/nats-top
 tar -xvf nats-top_0.5.3_darwin_amd64.tar.gz 2>/dev/null
 cp nats-top ..
 
+curl -sLJO https://github.com/ColinSullivan1/nats-simple-perf-tests/releases/download/v0.1.1/nats-simple-perf-tests_Linux_x86_64.tar.gz 
+tar -xvf nats-simple-perf-tests_Linux_x86_64.tar.gz 2>/dev/null
+cp natsthru ..
+
 cd ..
 rm -rf tmp
